48bfa7409bcb637e5666de5ba769df27a45b1848
bigwindlee <bigwindlee@hotmail.com>
2016-04-20 21:50:59 -0600

    loop directory

--- cpbuild.bat
+++ cpbuild.bat
@@ -1,6 +1,31 @@
 @echo off
 setlocal
 
+set DST_DIR=\\psbldfs\dfs\build\pt\pt856\856-106-R1\debug\WINX86
+
+set DIR_LIST=^
+psconfig.bat ^
+ActiveX      ^
+Apps         ^
+appserv      ^
+bin          ^
+build        ^
+class        ^
+lib          ^
+pgpsdk302    ^
+secvault     ^
+TUXEDO       ^
+utility      ^
+WEB          ^
+src
+
+
+
+for %%a in (%DIR_LIST%) do (
+    echo %%a
+)
+
+if exist %DST_DIR% dir %DST_DIR%
 
 
 
